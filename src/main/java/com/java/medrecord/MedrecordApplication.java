73aae30a549fee15b5f5f11539ae4912811b9362
Sudhir Takale <sudhirtakale99@gmail.com>
2023-10-07 09:47:26 +0530

    initial Commit

---
+++ src/main/java/com/java/medrecord/MedrecordApplication.java
@@ -0,0 +1,13 @@
+package com.java.medrecord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedrecordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedrecordApplication.class, args);
+	}
+
+}

a20716b953530c2b4b680f5b754964d6588c145f
Sudhir Takale <sudhirtakale99@gmail.com>
2023-11-04 10:58:24 +0530

    added patient code

--- src/main/java/com/java/medrecord/dao/AppointmentRepositery.java
+++ src/main/java/com/java/medrecord/dao/AppointmentRepositery.java
@@ -1,10 +1,9 @@
 package com.java.medrecord.dao;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.java.medrecord.entity.Appointment;
-import java.util.List;
-import java.util.Optional;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
 
 
 public interface AppointmentRepositery extends JpaRepository<Appointment, Integer>{

a37bde93db6b1fc78462f621bed88416cf49a89b
Sudhir Takale <sudhirtakale99@gmail.com>
2024-02-05 12:44:22 +0530

    Added appointment file

--- src/main/java/com/java/medrecord/controller/AdminHandler.java
+++ src/main/java/com/java/medrecord/controller/AdminHandler.java
@@ -1,14 +1,9 @@
 package com.java.medrecord.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 import com.java.medrecord.services.impl.AdminServicesImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
 
 
 
